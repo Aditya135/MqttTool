10c39caa30cf893491b60f59dad78224d60554ac
Aditya <rai.aditya135@gmail.com>
2019-03-19 02:46:06 +0530

    initial commit

--- MqttTool/Form1.cs
+++ MqttTool/Form1.cs
@@ -16,5 +16,15 @@ namespace MqttTool
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
